415eaf17fea9323551244a1f21c1e7509c77f520
samuel martins <samuel.martins4.sm@gmail.com>
2024-09-10 23:33:26 +0300

    chore: add the rest api urls

---
+++ backend/shop/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('product/<int:product_id>/', views.ProductDetailView.as_view(), name='product-detail'),
+    path('calculate-price/', views.CalculatePriceView.as_view(), name='calculate-price'),
+    path('add-to-cart/', views.AddToCartView.as_view(), name='add-to-cart'),
+    path('cart/', views.CartView.as_view(), name='cart'),
+    path('create-order/', views.CreateOrderView.as_view(), name='create-order'),
+    path('orders/', views.OrderListView.as_view(), name='order-list'),
+]
